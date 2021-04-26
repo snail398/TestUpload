47fbf2e2cdde7c4c9bdf5a2492506c09e3f085a3
RomaShtykov <lololo398@yandex.ru>
2021-04-26 22:23:38 +0300

    first

--- upload.py
+++ upload.py
@@ -1,4 +1,4 @@
 import yadisk
 y = yadisk.YaDisk(token="AQAAAAAFzz4bAAcWVJNhoT1G1E81liY-xuEtyr0")
-#y.mkdir("/Hello Word") # Создать папку
+y.mkdir("/Hello Word") # Создать папку
 y.upload("upload.py", "/Hello Word/upload.py") # Загружает первый файл
